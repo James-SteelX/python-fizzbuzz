cb445f6b8ce0e36c51eb2459676581fcfd950a14
James <James.Steel@me.com>
2016-12-19 13:48:08 +0000

    few more tests

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,11 +6,20 @@ def test_for_is_divisable():
 def test_for_number_divided_by_three():
     assert is_divisable_by_three(3) == True
 
+def test_for_number_not_divided_by_three():
+    assert is_divisable_by_three(2) == False
+
 def test_for_number_divided_by_five():
     assert is_divisable_by_five(5) == True
 
+def test_for_number_not_divided_by_five():
+    assert is_divisable_by_five(2) == False
+
 def test_for_number_divided_by_fifteen():
     assert is_divisable_by_fifteen(15) == True
+
+def test_for_number_not_divided_by_fifteen():
+    assert is_divisable_by_fifteen(2) == False
 
 def test_for_fizzbuzz_when_num_divisable_by_fifteen():
     assert fizzbuzz(15) == 'FizzBuzz'
