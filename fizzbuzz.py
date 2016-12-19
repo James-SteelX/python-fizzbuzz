23a7e0cc4232ed87b2cab9c43bbf4cc98a0caf61
James <James.Steel@me.com>
2016-12-19 13:45:47 +0000

    tests passing for fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,3 +9,12 @@ def is_divisable_by_five(num):
 
 def is_divisable_by_fifteen(num):
     return is_divisable(num, 15)
+
+def fizzbuzz(num):
+    if is_divisable_by_fifteen(num):
+        return 'FizzBuzz'
+    if is_divisable_by_three(num):
+        return 'Fizz'
+    if is_divisable_by_five(num):
+        return 'Buzz'
+    return num
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -11,3 +11,18 @@ def test_for_number_divided_by_five():
 
 def test_for_number_divided_by_fifteen():
     assert is_divisable_by_fifteen(15) == True
+
+def test_for_fizzbuzz_when_num_divisable_by_fifteen():
+    assert fizzbuzz(15) == 'FizzBuzz'
+
+def test_for_fizz_when_num_divisable_by_three():
+    assert fizzbuzz(3) == 'Fizz'
+
+def test_for_buzz_when_num_divisable_by_five():
+    assert fizzbuzz(5) == 'Buzz'
+
+def test_for_returning_number_when_not_divisable():
+    assert fizzbuzz(7) == 7
+
+def test_for_returning_number_when_not_divisable():
+    assert fizzbuzz(22) == 22
